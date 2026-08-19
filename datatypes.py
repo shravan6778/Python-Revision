d42f65ee52e868a0cb810e8a56e5277df936c875
shravan6778 <jaligamshravan2004@gmail.com>
2026-08-19 10:32:35 +0530

    Datatypes demonstration

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+#Python has a rich set of built-in data types. Every value in Python is an object belonging to a class.
+
+print(type(21))
+print(type(2.1))
+print(type("Sandy"))
+print(type(True))
+print(type(2+3j))
+print(type([1,2,3]))
+print(type((1,2)))
+print(type({1,2}))
+print(type({"a":1}))
+print(type(None))

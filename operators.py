d5d52f1a41aa718a76da7275c9226a6e22ad452d
shravan6778 <jaligamshravan2004@gmail.com>
2026-08-22 10:12:01 +0530

    Arithmetic Operators

---
+++ operators.py
@@ -0,0 +1,15 @@
+#Arithmetic Operators
+
+a, b = 20, 6
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# negative floor division
+print(-7 // 2)
+print(-7 % 2)
+

ee49ea8fb64c7800036bcedd1b8667e6612d6443
shravan6778 <jaligamshravan2004@gmail.com>
2026-08-22 10:15:55 +0530

    Relational / Comparisons operators

--- operators.py
+++ operators.py
@@ -13,3 +13,17 @@ print(a ** b)
 print(-7 // 2)
 print(-7 % 2)
 
+#Relational / Comparison Operators
+
+m, n = 10, 20
+print(m == n)
+print(m != n)
+print(m > n)
+print(m < n)
+print(m >= n)
+print(m <= n)
+
+#chained comparisons
+age = 22
+print(18 <= age < 30)
+
